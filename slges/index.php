f2107ccfe4e17f15e3a9cfa538f759044d8bd045
Conrad Esche <conrad.3rd@gmail.com>
2023-09-23 22:09:21 +0200

    some change

--- slges/index.php
+++ slges/index.php
@@ -62,7 +62,7 @@
                 <li><a href="01_viewer.php?id=<?php echo (rand(1,17009));?>">Viewer (random)</a></li>
                 <li>
                   <a href="01_gallery.php?funk=filter_exclude&exclude=9031,9143,379,381,384,9141,133,490,9099,358">
-                  Galerie</a>(Es werden 13180 Bilder geladen!)
+                  Galerie</a>(Es werden 13180 Bilder geladen!?)
                 </li>
                 <li><a href="allHash.php">Hashtags</a></li>
                 <li><a href="notes.php">Notizen</a></li>
