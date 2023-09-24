7bb44fbde252f11fa61589775509ffd0fc5137fc
Conrad Esche <conrad.3rd@gmail.com>
2023-09-24 16:48:19 +0200

    test2

--- index.php
+++ index.php
@@ -1,87 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="de">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>SLGES</title>
-  <link rel="shortcut icon" href="data:image/x-icon;" type="image/x-icon">
-
-  <!-- We need this line below for responsiveness! -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-
-  <!-- code a Hero / Banner section -->
-<div class="banner background2">
-  <div class="container">
-    <div class="mt5"><h1>Sammlung Esche</h1></div>
-    <div class="mt5"><p>Myanmar Bilder</p></div>
-    <div class="mt5"><a href="slges" class="btn btn-nuclear-green">Join now</a></div>
-
-
-
-  </div>
-</div>
-
-
-  <!-- code a "How it works" section -->
-<div class="container" style="margin:50px 0px 100px 0px; " >
-  <div class="carts">
-
-    <div class="cart">
-      <img src="images/apply.svg" alt="apply image">
-      <h2>Apply</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-
-    <div class="cart">
-      <img src="images/apply.svg" alt="apply image">
-      <h2>Apply</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-
-    <div class="cart">
-      <img src="images/apply.svg" alt="apply image">
-      <h2>Apply</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-
-    <div class="cart">
-      <img src="images/apply.svg" alt="apply image">
-      <h2>Apply</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-
-    <div class="cart">
-      <img src="images/apply.svg" alt="apply image">
-      <h2>Apply</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-
-  </div>
-</div>
-  <!-- code a footer -->
-  <div class="container" id="footer">
-    <h2>Follow me or not</h2>
-    <ul class="list-inline" style="margin-bottom:0px; padding-bottum: 0px;">
-      <li>
-        <a href="https://github.com/Conrad3rd" target="_blank">
-          <i class="fab fa-github-square"></i>
-        </a>
-      </li>
-      <li>
-        <a href="#" target="_blank">
-          <i class="fab fa-facebook-square"></i>
-        </a>
-      </li>
-      <li>
-        <a href="" target="_blank">
-          <i class="fab fa-instagram"></i>
-        </a>
-      </li>
-    </ul>
-  </div>
-</div>
+  <h1>Sammlung Esche</h1>
 </body>
 </html>
