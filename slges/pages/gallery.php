35862a259d4341be61325aca18a69148ec77e06a
Conrad Esche <conrad.3rd@gmail.com>
2023-10-01 16:15:43 +0200

    paginator + linking

--- slges/pages/gallery.php
+++ slges/pages/gallery.php
@@ -2,10 +2,10 @@
 <?php include("./elements/header.php");?>
 
 
-  <h1>PDO Test</h1>
+  <h1>PDO Test2</h1>
 
   <?php
-    $res = fetch_posts();
+    $res = fetch_images();
   ?>
 
   <table>
@@ -13,8 +13,12 @@
     <?php foreach ($res as $row) : ?>
       <tr>
         <td><?php echo "$row[id]";?></td>
-        <td><?php echo "$row[pfad]";?></td>
-      </tr>
+        <td>
+          <a href="../sl_Esche/L/<?php echo "$row[pfad]";?>">
+            <?php echo "$row[pfad]";?>
+          </a>
+          </td>
+        </tr>
     <?php endforeach ?>
 
   </table>
