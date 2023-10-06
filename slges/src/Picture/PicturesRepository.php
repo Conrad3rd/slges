0f57522096b15b05ec5fbe87aec7e397f0370e46
Conrad Esche <conrad.3rd@gmail.com>
2023-10-06 21:48:25 +0200

    add pic repo class

---
+++ slges/src/Picture/PicturesRepository.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Picture;
+
+class PicturesRepository {
+  function fetchImages(int $ipp = 21, int $pageNumer = 20) {
+    global $pdo;
+
+    // $ipp = 22; // image per page
+    // $image_id = 95;
+    // $image_id = $image_id - ($ipp / 2);
+
+    $stmt = $pdo->prepare("SELECT * FROM Bilder ORDER BY id ASC LIMIT :ipp OFFSET :pageNumer");
+    $stmt->execute(['pageNumer' => $pageNumer, 'ipp' => $ipp]);
+    return $stmt;
+  }
+
+  function fetchImage($id) {
+    global $pdo;
+
+    $stmt = $pdo->prepare("SELECT * FROM `Bilder` WHERE id = :id");
+    $stmt->execute(['id' => $id]);
+    return $stmt->fetch();
+  }
+}
+
+?>
